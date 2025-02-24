849c1a53b18e157f679bdf6a4e7e454a392f9e7f
SteinCode <rokas.student@gmail.com>
2025-02-24 12:30:00 +0200

    url fix

--- Library/SCMerchantClient/Config.php
+++ Library/SCMerchantClient/Config.php
@@ -7,7 +7,7 @@ namespace Spectrocoin\Merchant\Library\SCMerchantClient;
 class Config
 {
     const MERCHANT_API_URL = 'https://spectrocoin.com/api/public';
-    const AUTH_URL = 'https://s0pectrocoin.com/api/public/oauth/token';
+    const AUTH_URL = 'https://spectrocoin.com/api/public/oauth/token';
     const PUBLIC_SPECTROCOIN_CERT_LOCATION = 'https://spectrocoin.com/files/merchant.public.pem';
     const ACCEPTED_FIAT_CURRENCIES = ["EUR", "USD", "PLN", "CHF", "SEK", "GBP", "AUD", "CAD", "CZK", "DKK", "NOK"];
 
